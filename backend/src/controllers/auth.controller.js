d685d7c334ed9813e693599c5c54fcb78bb57ab2
savvytony <savvy.tony@savvycom.dev>
2024-05-20 18:33:01 +0700

    feat(backend): update auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -11,12 +11,16 @@ export const login = asyncHandler(async (req, res) => {
   const user = await authService.login(req.body);
   const accessToken = await tokenService.signAccessToken(user);
 
+  user.password = undefined;
+
   return res.status(httpStatus.OK).json(apiResponse(httpStatus.OK, 'Login successfully', { accessToken, user }));
 });
 
 export const register = asyncHandler(async (req, res) => {
   const user = await userService.createUser(req.body);
   const accessToken = await tokenService.signAccessToken(user);
+
+  user.password = undefined;
 
   return res.status(httpStatus.OK).json(apiResponse(httpStatus.OK, 'Register successfully', { accessToken, user }));
 });
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -7,9 +7,8 @@ import taskRoute from './task.route.js';
 const router = express.Router();
 
 router.use('/auth', authRoute);
-// router.use(authentication);
+router.use(authentication);
 router.use('/posts', postRoute);
 router.use('/tasks', taskRoute);
-
 
 export default router;
